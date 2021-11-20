42252aa94fbb12b4a96b3a06831ca52a6d261c29
youqinSun <xiaoouvivian.it@gmail.com>
2021-11-20 19:35:25 +0200

    part2 2.02

---
+++ part2/2.02/fe-todos/todo-app/src/services/ToDoService.js
@@ -0,0 +1,33 @@
+  
+import axios from 'axios'
+const baseUrl = 'http://localhost:8081/'
+const todoUrl = baseUrl + "todos"
+
+const getToDos = async () => {
+    const request = axios.get(todoUrl)
+    const response = await request
+    return response.data
+}
+
+const getImage = async () => {
+    const request = axios.get("https://picsum.photos/1200", {
+        responseType: 'arraybuffer'})
+     
+    const response = await request
+    return response.data
+}
+
+
+const createToDo = async newObject => {
+    console.log("service: createToDo");
+    const request = axios.post(todoUrl, newObject)
+    const response = await request
+    return response.data
+}
+
+
+export default {
+    getToDos,
+    createToDo,
+    getImage
+}
