1696249fe4ef96f229c040af34bcd5e3e29971d7
youqinSun <xiaoouvivian.it@gmail.com>
2021-11-25 00:33:00 +0200

    part3 3.02

---
+++ part3/3.02/logoutput/src/main/java/com/youqin/logoutput/Controller.java
@@ -0,0 +1,33 @@
+package com.youqin.logoutput;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private StatusService service;
+
+    @GetMapping("/")
+    public String hello() {
+        return "Greetings from Logoutput app";
+    }
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return service.getStatus();
+    }
+
+
+}
