61b3160dd35378564c097094155f0229fe1ab4d7
youqinSun <xiaoouvivian.it@gmail.com>
2021-11-28 12:52:01 +0200

    part3 3.04

---
+++ part3/3.04/be-todos/src/main/java/com/sunyouqin/backendtodos/BackendtodosApplication.java
@@ -0,0 +1,13 @@
+package com.sunyouqin.backendtodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtodosApplication.class, args);
+	}
+
+}
---
+++ part3/3.04/be-todos/src/main/java/com/sunyouqin/backendtodos/Todo.java
@@ -0,0 +1,43 @@
+package com.sunyouqin.backendtodos;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
