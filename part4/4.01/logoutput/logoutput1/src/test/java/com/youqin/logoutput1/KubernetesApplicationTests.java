a97daf6c38a28588b273e6fab07e168911a1b030
youqinSun <xiaoouvivian.it@gmail.com>
2021-12-03 00:35:33 +0200

    part4 4.01

---
+++ part4/4.01/logoutput/logoutput1/src/test/java/com/youqin/logoutput1/KubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youqin.logoutput1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
