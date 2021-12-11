68f7877d698236978efe2541d71e66616910659a
youqinSun <xiaoouvivian.it@gmail.com>
2021-12-11 21:37:48 +0200

    part5 5.01

---
+++ part5/5.01/dummysite/index.js
@@ -0,0 +1,29 @@
+
+const express = require("express");
+const axios = require('axios');
+require('dotenv').config();
+//const website_url = process.env.WEBSITE_URL;
+const website_url = "https://example.com/";
+//const website_url = "https://devopswithkubernetes.com/";
+
+const app = express();
+const port = process.env.PORT || "3000";
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+
+app.get("*", (req, res) => {
+    axios({
+        url: website_url,
+        method: 'GET',
+        responseType: 'text'
+    }).then(response => {
+        console.log(response.status);
+        res.send(response.data)
+    }).catch(err => {
+        throw err
+    });
+});
+
